83a6f0358b1e5c1261e9601474d0024f4baa245b
VisnuDev <87351344+VisnuDev@users.noreply.github.com>
2022-10-14 13:27:07 +0530

    Add files via upload

---
+++ Task4-github_action/app.py
@@ -0,0 +1,20 @@
+
+##flask app to print "Hello Visnu"
+
+from flask import Flask
+import pandas as pd
+import numpy as np
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return 'Hello Visnu'
+
+@app.route('/hel/')
+def hello():
+    return 'My name is Visnu'
+
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0",port=5000)
